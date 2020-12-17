3f61f95b4084b01711f0bb6343b48aabe3a31557
Mohammad Ataei <mammadataei@gmail.com>
2020-12-17 11:32:43 +0330

    refactor buildComponent

--- packages/crayon/src/plugin/buildComponent.ts
+++ packages/crayon/src/plugin/buildComponent.ts
@@ -2,15 +2,15 @@ import { Styles, TailwindComponent } from '../types'
 import resolveGenerator from './resolveGenerator'
 
 export default function buildComponent (name: string, styles: Styles): TailwindComponent {
-  let componentStyles = {}
-
-  for (const [property, value] of Object.entries(styles)) {
-    const generator = resolveGenerator(property)
-    const cssStyle = generator(value)
-    componentStyles = { ...componentStyles, ...cssStyle }
-  }
-
   return {
-    [name]: componentStyles,
+    [name]: buildComponentStyles(styles),
   }
+}
+
+function buildComponentStyles (styles: Styles) {
+  return Object.entries(styles)
+    .reduce((componentStyles, [property, value]) => {
+      const generator = resolveGenerator(property)
+      return Object.assign(componentStyles, generator(value))
+    }, {})
 }
