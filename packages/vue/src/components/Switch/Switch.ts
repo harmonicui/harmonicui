eba96d27fcce213dab2065dae4e433f9630df896
MloDehghani <amirdehghani137722@gmail.com>
2021-06-08 20:24:40 +0430

    add Switch Component

---
+++ packages/vue/src/components/Switch/Switch.ts
@@ -0,0 +1,130 @@
+import { computed, defineComponent, ref } from 'vue'
+import { useGenerateDataIsAttribute, useGenerateId } from '../../composable'
+import { render } from '../../utils'
+import {
+  HelperTextContract,
+  LabelContract,
+  SwitchToggleContract,
+  provideHelperTextContext,
+  provideLabelContext,
+  provideSwitchToggleContext,
+  provideSwitchThumbContext,
+} from '../../contexts'
+
+export default defineComponent({
+  name: 'Switch',
+
+  inheritAttrs: false,
+
+  props: {
+    as: {
+      type: String,
+      default: 'fragment',
+      validator: (value: string) => {
+        return ['div', 'fragment'].indexOf(value) !== -1
+      },
+    },
+
+    toggleId: {
+      type: String,
+      default: () => useGenerateId('SwitchToggle'),
+    },
+
+    labelId: {
+      type: String,
+      default: () => useGenerateId('SwitchLabel'),
+    },
+
+    helperTextId: {
+      type: String,
+      default: () => useGenerateId('SwitchHelperText'),
+    },
+
+    value: {
+      type: String,
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+
+    modelValue: {
+      type: Boolean,
+    },
+  },
+
+  emits: ['update:modelValue'],
+
+  setup(props, { slots, emit, attrs }) {
+    const refs = {
+      toggle: ref<SwitchToggleContract['ref']['value']>(null),
+      label: ref<LabelContract['ref']['value']>(null),
+      helperText: ref<HelperTextContract['ref']['value']>(null),
+    }
+
+    function toggleValue() {
+      emit('update:modelValue', !props.modelValue)
+    }
+
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.key === 'Space') {
+        toggleValue()
+      }
+    }
+
+    function resolveDataIs() {
+      return computed(() => {
+        return props.disabled ? 'disabled' : props.modelValue ? 'on' : 'off'
+      })
+    }
+
+    provideSwitchToggleContext({
+      toggleValue,
+      handleKeyDown,
+      ref: refs.toggle,
+      id: props.toggleId,
+      tabindex: '0',
+      role: 'switch',
+      disabled: props.disabled,
+      value: props.value,
+      modelValue: computed(() => props.modelValue),
+      'aria-describedby': computed(() => {
+        return refs.helperText.value ? props.helperTextId : undefined
+      }),
+      'aria-checked': computed(() => props.modelValue),
+      'aria-labelledby': computed(() => props.labelId),
+    })
+
+    provideLabelContext({
+      ref: refs.label,
+      id: props.labelId,
+      for: computed(() => undefined),
+      'data-is': resolveDataIs(),
+      ...useGenerateDataIsAttribute({
+        disabled: props.disabled,
+      }),
+    })
+
+    provideHelperTextContext({
+      ref: refs.helperText,
+      id: props.helperTextId,
+      hidden: undefined,
+    })
+
+    provideSwitchThumbContext({
+      tabindex: '-1',
+      'data-is': resolveDataIs(),
+    })
+
+    return () =>
+      render({
+        as: props.as,
+        props: attrs,
+        children: slots.default,
+        childrenProps: {
+          disabled: props.disabled,
+        },
+      })
+  },
+})
